db18da3fac6a7957ca53757fcaf6602fb87faa4c
MicahlKnotek <gracajnborowski@gmail.com>
2023-11-06 10:36:08 +0100

    commit1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+// sadanie 1 zapis do pliku txt linii czytanej z zklawiatury
+int main()
+{
+    char znaki[100];
+    fstream plik;
+    plik.open("Michal.txt", ios::in | ios::out | ios::trunc);
+
+    cout << "Podaj wpis: " << "\n";
+    cin.getline(znaki, 100, "\n")
+
+    plik << znaki;
+   
+    plik.close();
+    system("pause")
+    EXIT_SUCCESS;
+}
